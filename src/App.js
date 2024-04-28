ecccde7379a54e3e80ce758e284bcfc6b15ae9aa
Muhammad Yasin <m.yaseensalim@gmail.com>
2024-04-28 10:47:03 -0700

    second

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import './App.css';
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchdata } from './api';
+
 function App() {
-  const [data,setdata]=useState("");
-  useEffect(()=>{
-    fetch('https://api.adviceslip.com/advice').then(response=>response.json()).then(data=>console.log(data.slip.advice))
-  })
+  const [data, setdata] = useState("");
+
+  useEffect(() => {
+    const fetchDataAndSetAdvice = async () => {
+      const advice = await fetchdata();
+      setdata(advice);
+    };
+
+    fetchDataAndSetAdvice();
+  }, []);
+
+  const handleButtonClick = async () => {
+    const advice = await fetchdata();
+    setdata(advice);
+  };
+
   return (
     <div className="App">
       <div className='card'>
-<header>Hello</header>
+        <p>{data}</p>
+        <button className='button' onClick={handleButtonClick}>Give Me Advice!</button>
       </div>
-
     </div>
-   
   );
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,7 @@
-export const fetchdata=async()=>{
-    const response=await fetch('https://api.adviceslip.com/advice');
-    return await response.json();
-}
+export const fetchdata = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    const advice = data.slip.advice;
+    return advice;
+  };
+  
